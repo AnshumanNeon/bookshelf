9c2c3d96bad76d62bf30b92e259706d835c5255d
AnshumanNeon <anshumanneon@gmail.com>
2024-03-10 16:29:26 +0530

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{
+    event::{self, KeyCode, KeyEventKind},
+    terminal::{
+        disable_raw_mode, enable_raw_mode, EnterAlternateScreen,
+        LeaveAlternateScreen
+    },
+    ExecutableCommand
+};
+
+use ratatui::{
+    prelude::{CrosstermBackend, Stylize, Terminal},
+    widgets::Paragraph
+};
+
+use std::io::{Result, stdout};
+
+fn main() -> Result<()> {
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut term = Terminal::new(CrosstermBackend::new(stdout()))?;
+    term.clear()?;
+
+    loop {
+        term.draw(|frame| {
+            let area = frame.size();
+            frame.render_widget(
+                Paragraph::new("hello").white().on_blue(),
+                area
+            );
+        })?;
+
+        if event::poll(std::time::Duration::from_millis(16))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q')
+                {
+                    break;
+                }
+            }
+        }
+    }
+    
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
